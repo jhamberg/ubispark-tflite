9d4698898562e24a782ffad561f1613d85de4129
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2018-10-29 15:09:05 +0200

    Basic Android SDK 14+ project with tflite and kryonet as dependencies

---
+++ app/src/main/java/fi/helsinki/cs/hamberg/mobster_tflite/ControlActivity.java
@@ -0,0 +1,13 @@
+package fi.helsinki.cs.hamberg.mobster_tflite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control);
+    }
+}
