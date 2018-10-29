9b2c3f1af210e893c51d43f66c192557d15b19e1
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2018-10-30 01:06:44 +0200

    Start the service once battery optimizations have been ignored

--- client/app/src/main/java/fi/helsinki/cs/hamberg/mobster_tflite/ControlActivity.java
+++ client/app/src/main/java/fi/helsinki/cs/hamberg/mobster_tflite/ControlActivity.java
@@ -1,13 +1,45 @@
 package fi.helsinki.cs.hamberg.mobster_tflite;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ControlActivity extends AppCompatActivity {
 
+    @SuppressLint("BatteryLife")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
+        // moveTaskToBack(true);
+    }
+
+    @SuppressLint("BatteryLife")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+            if(powerManager != null && !powerManager.isIgnoringBatteryOptimizations(getPackageName())) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+            } else {
+                Intent serviceIntent = new Intent(this, BackgroundService.class);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(serviceIntent);
+                } else {
+                    moveTaskToBack(true);
+                    startService(serviceIntent);
+                }
+                finish();
+            }
+        }
     }
 }
