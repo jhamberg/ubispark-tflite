60b79e0f18129d0818b486782e21af21e5bfdb58
Jonatan Hamberg <jonatan.hamberg@cs.helsinki.fi>
2019-04-23 18:36:09 +0300

    Frozen pb model requires proportional color values

--- client/desktop/src/main/java/FileUtils.java
+++ client/desktop/src/main/java/FileUtils.java
@@ -2,7 +2,9 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,14 +14,10 @@ import java.util.List;
  * University of Helsinki
  */
 
-@SuppressWarnings("SameParameterValue")
+@SuppressWarnings({"SameParameterValue", "FieldCanBeLocal"})
 class FileUtils {
     private static final int CHANNELS = 3;
     private static final int BUFFER_SIZE = 16384;
-
-    // Keep a reference to avoid reallocation cost
-    @SuppressWarnings("FieldCanBeLocal")
-    private static FloatBuffer imgData;
 
     static byte[] loadBytes(String path) throws IOException {
         InputStream is = FileUtils.class.getClassLoader().getResourceAsStream(path);
@@ -39,20 +37,21 @@ class FileUtils {
         InputStream is = FileUtils.class.getClassLoader().getResourceAsStream(path);
         assert (is != null);
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(is, "utf8"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
         String line;
         while ((line = br.readLine()) != null) {
             result.add(line);
         }
+        br.close();
         return result;
     }
 
     static BufferedImage loadImage(String path) throws IOException {
         return ImageIO.read(FileUtils.class.getResourceAsStream(path));
     }
-    
+
     static FloatBuffer imageToFloatBuffer(BufferedImage image) {
-        imgData = FloatBuffer.allocate(image.getWidth() * image.getHeight() * CHANNELS);
+        FloatBuffer imgData = FloatBuffer.allocate(image.getWidth() * image.getHeight() * CHANNELS);
 
         // Reference is reused, need to overwrite
         imgData.rewind();
@@ -61,10 +60,12 @@ class FileUtils {
         for (int row = 0; row < image.getHeight(); row++) {
             for (int column = 0; column < image.getWidth(); column++) {
                 final int val = image.getRGB(column, row);
-                imgData.put(index++, (val >> 16) & 0xFF);
-                imgData.put(index++, (val >> 8) & 0xFF);
-                imgData.put(index++, val & 0xFF);
+                // Note, this model expects colors in 0-1 range unlike the tflite model
+                imgData.put(index++, ((val >> 16) & 0xFF) / 255.0f);
+                imgData.put(index++, ((val >> 8) & 0xFF) / 255.0f);
+                imgData.put(index++, (val & 0xFF) / 255.0f);
             }
+            System.out.println();
         }
 
         return imgData;
